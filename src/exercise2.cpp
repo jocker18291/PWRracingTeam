b4c8af5bdb3b83b35ebedc2353f02b7e243ece08
jocker18291 <dawidans714@gmail.com>
2025-10-15 23:53:57 +0200

    Fully initialized convex hull method

--- src/exercise2.cpp
+++ src/exercise2.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "../include/exercise2.hpp"
 
 using namespace std;
 
-int Point::angle(Point a, Point b, Point c) {
+int Point::angle(Point a, Point b, Point c) { //method that calculates the orientation between points
     float area = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 
     if(area < 0) return -1; // clockwise
@@ -14,11 +15,11 @@ int Point::angle(Point a, Point b, Point c) {
     return 0;
 }
 
-double Point::dist(Point a, Point b) {
+double Point::dist(Point a, Point b) { //calculating the square of a distance
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
-vector<vector<int>> Point::convexHull(vector<vector<int>> points, int n) {
+vector<vector<double>> Point::convexHull(vector<vector<int>> points, int n) {
     if (n < 3) return {{-1}}; // convex hull not possible with fewer than 3 points
 
     vector<Point> a; //converting 2d points into vector of Point type
@@ -26,5 +27,31 @@ vector<vector<int>> Point::convexHull(vector<vector<int>> points, int n) {
         a.push_back({(double)p[0], (double)p[1]});
     }
 
+    Point p0 = *min_element(a.begin(), a.end(), [](Point a, Point b) { //looking for the 'lowest' starting point, if there is a double - choose the one on the most left
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+    });
 
+    sort(a.begin(), a.end(), [&p0](Point a, Point b) { // sorting the vector based on polar angle
+        int o = Point::angle(p0, a, b);
+        if(o ==0) { //if two points are collinear -> keep farther one last
+            return Point::dist(p0, a) < Point::dist(p0, b);
+        }
+        return o < 0; //sort counter-clockwise
+    });
+
+    vector<Point> hull; //creating vector to store the convex hull
+    for(auto& cur : a) { // iterating through all points
+        while (hull.size() > 1 && angle(hull[hull.size() - 2], hull.back(), cur) >= 0) { //if there is a point where it is not a left turn -> pop the middle element
+            hull.pop_back();
+        }
+        hull.push_back(cur); //add the current point to the convex hull
+    }
+
+    if(hull.size() < 3) return {{-1}}; // there cannot be a convex hull with fewer than 3 elements
+
+    vector<vector<double>> res;
+    for(auto& it : hull) //creating vector as a result to return
+        res.push_back({(double)it.x, (double)it.y});
+    
+    return res;
 }
