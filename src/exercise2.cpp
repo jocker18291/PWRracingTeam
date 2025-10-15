9836cdf72d3d46a9d22f3552cd362e5ff6cae0a1
jocker18291 <dawidans714@gmail.com>
2025-10-15 17:30:01 +0200

    Initialized angle method

--- src/exercise2.cpp
+++ src/exercise2.cpp
@@ -3,3 +3,12 @@
 
 using namespace std;
 
+int Point::angle(Point a, Point b, Point c) {
+    float area = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+
+    if(area < 0) return -1; // clockwise
+
+    else if (area > 0) return 1; // counter-clockwise
+
+    return 0;
+}
