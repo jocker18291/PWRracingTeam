3e495c44006aa29f6a4f35f386ddae11232d78a4
jocker18291 <dawidans714@gmail.com>
2025-10-15 21:20:57 +0200

    Initializing convexHull method

--- src/exercise2.cpp
+++ src/exercise2.cpp
@@ -17,3 +17,14 @@ int Point::angle(Point a, Point b, Point c) {
 double Point::dist(Point a, Point b) {
     return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
+
+vector<vector<int>> Point::convexHull(vector<vector<int>> points, int n) {
+    if (n < 3) return {{-1}}; // convex hull not possible with fewer than 3 points
+
+    vector<Point> a; //converting 2d points into vector of Point type
+    for(auto& p : points) {
+        a.push_back({(double)p[0], (double)p[1]});
+    }
+
+
+}
