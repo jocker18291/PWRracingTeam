9262b9f706e4cd4f734d8cbe6ace1f54e829e37f
jocker18291 <dawidans714@gmail.com>
2025-10-17 02:23:11 +0200

    Create convex hull in main

--- src/main.cpp
+++ src/main.cpp
@@ -18,4 +18,21 @@ int main(int argc, char* argv[]) {
         cout << "Couldn't open the file: " << filename << endl;
         return 1;
     }
+
+    int n;
+    file >> n; //we read the first line
+    vector<vector<double>> points(n, vector<double>(2)); //we create a vector of size n x 2
+
+    for(int i = 0; i < n; i++) {
+        file >> points[i][0] >> points[i][1];
+    }
+    file.close();
+
+    //Convex Hull
+    auto hull = Point::convexHull(points, n);
+    cout << "Convex Hull: ";
+    for(auto& p : hull) {
+        cout << "(" << p[0] << ", " << p[1] << ") ";
+    }
+    cout << endl;
 }
